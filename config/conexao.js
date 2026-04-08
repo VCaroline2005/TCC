2b63c9183943702219ad99f45cd51e73876df508
VCaroline2005 <vividias485@gmail.com>
2026-04-08 16:56:26 -0300

    08/04

--- config/conexao.js
+++ config/conexao.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const url = "mongodb+srv://vcaroline1507:vcaroline1507@cluster0.uww0rts.mongodb.net/";
+const url = "mongodb+srv://vcaroline1507:<@Lc124550>@cluster0.uww0rts.mongodb.net/?appName=Cluster0";
 const conexao = await mongoose.connect('mongodb://localhost:27017/ifstore')
 
 export default conexao
