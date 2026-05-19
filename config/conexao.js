039853e21074a49d641de771575d11571b86fd2c
VCaroline2005 <vividias485@gmail.com>
2026-05-19 17:44:47 -0300

    19/05

--- config/conexao.js
+++ config/conexao.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/ifstore'
+const uri = process.env.MONGODB_URI || 'mongodb+srv://vcaroline1507_db_user:12345@cluster0.1uswese.mongodb.net/?appName=Cluster0'
 const conexao = await mongoose.connect(uri)
 
-export default conexao
+export default conexao;
